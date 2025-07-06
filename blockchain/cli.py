de696bd70f31d914d04a47b6fa649fe0f097a52b
Bekmukhamed <bekmukhamed.tursunbayev@gmail.com>
2025-07-05 20:17:20 -0500

    options

--- blockchain/cli.py
+++ blockchain/cli.py
@@ -1,6 +1,30 @@
+import sys
+from config import Config
+
+options = {
+    'n': 'nodes',
+    'm': 'neighbors',
+    'k': 'miners',
+    'h': 'hashrate',
+    't': 'blocktime',
+    'd': 'difficulty',
+    'r': 'reward',
+    'w': 'wallets',
+    'x': 'transactions',
+    'i': 'interval',
+    's': 'blocksize',
+    'l': 'blocks',
+    'p': 'print',
+    'g': 'debug'
+}
+
 def parse_args(argv):
-    
+    args = {}
     i = 0
     while (i < len(argv)):
-        token = argv[i]
-        
+        if argv[i].startswith('--'):
+            key = argv[i][2:]
+    
+
+    argv = sys.argv[1:]
+    parse_args(argv)
--- blockchain/config.py
+++ blockchain/config.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 @dataclass
 class Config:
     nodes: int = 10
+    neighbors: int = 3
     miners: int = 2
     hashrate: int = 1000
     blocktime: int = 10
@@ -14,12 +15,14 @@ class Config:
     interval: int = 1
     blocksize: int = 1000
     blocks: int = 10
-    print_blocks: bool = False
+    print: bool = False
     debug: bool = False
 
     def __post_init__(self):
         if self.nodes < self.miners:
             raise ValueError("Number of miners cannot exceed number of nodes.")
+        if self.neighbors < 0:
+            raise ValueError("Number of neighbors cannot be negative.")
         if self.hashrate <= 0:
             raise ValueError("Hashrate must be a positive integer.")
         if self.blocktime <= 0:
@@ -38,13 +41,13 @@ class Config:
             raise ValueError("Block size must be a positive integer.")
         if self.blocks <= 0:
             raise ValueError("Number of blocks must be a positive integer.")
-        if self.print_blocks not in [True, False]:
+        if self.print not in [True, False]:
             raise ValueError("Print blocks must be a boolean value.")
         if self.debug not in [True, False]:
             raise ValueError("Debug mode must be a boolean value.")
     def __str__(self):
-        return (f"Config (nodes={self.nodes}, miners={self.miners}, hashrate={self.hashrate}, "
+        return (f"Config (nodes={self.nodes}, neighbors={self.neighbors}, miners={self.miners}, hashrate={self.hashrate}, "
                 f"blocktime={self.blocktime}, difficulty={self.difficulty}, reward={self.reward}, "
                 f"wallets={self.wallets}, transactions={self.transactions}, interval={self.interval}, "
-                f"blocksize={self.blocksize}, blocks={self.blocks}, print_blocks={self.print_blocks}, "
+                f"blocksize={self.blocksize}, blocks={self.blocks}, print={self.print}, "
                 f"debug={self.debug})")
